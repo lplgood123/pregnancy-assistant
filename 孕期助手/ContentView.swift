3b6b1cb80b465b706b859ee96d4530aa126209e4
Lipeilin <lplgood@gmail.com>
2026-02-15 21:48:09 +0800

    Initial Commit

---
+++ 孕期助手/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  孕期助手
+//
+//  Created by spectrex on 2026/2/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
